66b3f7e39f62bf3db1f5f87e50b9872c9e1c2f7c
Ajay <ajayashrestha105@gmail.com>
2023-07-04 10:39:25 +0545

    latest on backend

---
+++ routes/adminBro/admin.router.js
@@ -0,0 +1,53 @@
+const AdminBro = require('admin-bro')
+const AdminBroExpress = require('@admin-bro/express')
+
+const AdminBroMongoose = require('@admin-bro/mongoose')
+
+
+
+AdminBro.registerAdapter(AdminBroMongoose)
+const mongoose = require('mongoose');
+const User = require('../../model/User');
+const Pet = require('../../model/Pet');
+const promotion = require('../../model/promotion');
+
+
+
+const adminBro = new AdminBro({
+    // databases: [mongoose],
+    rootPath: '/admin',
+    resources: [
+        {
+            resource: User,
+            options: {
+                parent: {
+                    name: 'Database',
+                    icon: 'fas fa-database',
+                },
+            },
+        },
+        {
+            resource: Pet,
+            options: {
+                parent: {
+                    name: 'Database',
+                    icon: 'fas fa-database',
+                },
+            },
+        },
+        {
+            resource: promotion,
+            options: {
+                parent: {
+                    name: 'Database',
+                    icon: 'fas fa-database',
+                },
+            },
+        },
+    ],
+
+
+})
+
+const router = AdminBroExpress.buildRouter(adminBro)
+module.exports = router;
---
+++ utils/cloudinary.js
@@ -0,0 +1,12 @@
+
+
+const cloudinary = require('cloudinary');
+
+
+cloudinary.config({
+    cloud_name: 'dutnwvkf8',
+    api_key: '268148784685133',
+    api_secret: 'Lg44NDcX9Wb65ktz_TFClSqLMnY'
+});
+
+module.exports = cloudinary;
